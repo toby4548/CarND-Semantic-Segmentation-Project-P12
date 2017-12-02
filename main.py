dd29016c2897645d6065b9c486579801c16ed7cc
Tony Huang <toby4548@gmail.com>
2017-12-02 21:03:12 +0100

    Finish load model function

--- main.py
+++ main.py
@@ -32,8 +32,16 @@ def load_vgg(sess, vgg_path):
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
+
+    tf.saved_model.loader.load(sess,[vgg_tag],vgg_path)
+    graph = tf.get_default_graph()
+    w1 = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    w4 = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    w5 = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    w8 = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
     
-    return None, None, None, None, None
+    return w1, keep, w4, w5, w8
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -47,6 +55,11 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
+    conv_1x1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding='same', kernel_regularizer = tf.contrib.layers.l2_regularizer(1e-3))
+    output = tf.layers.conv2d_transpose(conv_1x1, num_classes, 4, 2, padding="smae", kernel_regularizer = tf.contrib.layer.l2_regularizer(1e-3))
+
+
+
     return None
 tests.test_layers(layers)
 
@@ -81,6 +94,11 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param learning_rate: TF Placeholder for learning rate
     """
     # TODO: Implement function
+    for epochs in epochs:
+        for image, label in get_batches_fn(batch_size):
+            #Training
+            pass
+    
     pass
 tests.test_train_nn(train_nn)
 
@@ -109,7 +127,8 @@ def run():
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
         # TODO: Build NN using load_vgg, layers, and optimize function
-
+        input_image, keep_prob, layer3_out,layer4_out,layer7_out =load_vgg(sess, vgg_path)
+        layer_output = layers(layer3_out,layer4_out,layer7_out,num_classes)
         # TODO: Train NN using the train_nn function
 
         # TODO: Save inference data using helper.save_inference_samples
